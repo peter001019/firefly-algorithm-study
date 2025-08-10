1aa2ac24c4210bf1ecfd5d3b171bda81f2c91d4a
한승우 <peter001019@naver.com>
2025-08-10 23:47:54 +0900

    2251 물통

--- 2주차/한승우/2251 물통.cpp
+++ 2주차/한승우/2251 물통.cpp
@@ -2,34 +2,44 @@
 using namespace std;
 
 int A, B, C;
+bool visited[201][201][201];
+set<int> ans;
 
-void sol(){
+void dfs(int a, int b, int c){
+    if(visited[a][b][c]) return;
 
+    if(a == 0) {
+        ans.insert(c);
+    }
+
+    visited[a][b][c] = true;
+
+    int aGap = A - a;
+    int bGap = B - b;
+    int cGap = C - c;
+
+    //C -> A
+    if(c > 0) dfs(a + min(aGap, c), b, max(0, c - aGap));
+    //C -> B
+    if(c > 0) dfs(a, b + min(bGap, c), max(0, c - bGap));
+    //A -> C
+    if(a > 0) dfs(max(0, a - cGap), b, c + min(cGap, a));
+    //A -> B
+    if(a > 0) dfs(max(0, a - bGap), b + min(bGap, a), c);
+    //B -> A
+    if(b > 0) dfs(a + min(aGap, b), max(0, b - aGap), c);
+    //B -> C
+    if(b > 0) dfs(a, max(0, b - cGap), c + min(cGap, b));
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    cin >> A >> B >> C;    
+    cin >> A >> B >> C;
 
-    sol();
+    dfs(0, 0, C);
+
+    for(int a: ans) cout << a << " ";
 
     return 0;
 }
-
-/*
-8 9 10
-
-0 0 10
-0 9 1
-
-8 1 1
-8 2 0
-0 2 8
-8 0 2
-
-0 8 2
-0 1 9
-
-10, 1, 2, 8, 9
-*/
