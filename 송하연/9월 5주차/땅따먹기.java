722e7fae1269621f73443b755c60feb40e4a42eb
hanni <102026726+hysong4u@users.noreply.github.com>
2025-10-01 04:37:39 +0900

    [0930] 땅따먹기

---
+++ 송하연/9월 5주차/땅따먹기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        int n= land.length;
+        
+        for(int i=1; i<n; i++){
+            land[i][0] += Math.max(land[i-1][1],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        }
+                
+        for(int i=0; i<4; i++){
+            answer = Math.max(answer,land[n-1][i]);
+        }
+        
+        return answer;
+    } 
+}
