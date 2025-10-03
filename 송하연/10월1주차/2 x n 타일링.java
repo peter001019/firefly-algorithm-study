773c68197e1c73b396e2df47a7dc31f7fef6ba01
hanni <102026726+hysong4u@users.noreply.github.com>
2025-10-03 20:49:10 +0900

    [10/03]2 x n 타일링
    Implement dynamic programming solution for 2 x n tiling problem.

---
+++ 송하연/10월1주차/2 x n 타일링.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+  
+        int[] dp = new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            //System.out.println(i+":"+dp[i]);
+            dp[i] = (dp[i-1]+dp[i-2])%1000000007;
+        }
+    
+        return answer=dp[n];
+ 
+    }
+    
+   
+}
