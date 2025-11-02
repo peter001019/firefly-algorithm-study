afd063a2a11f6720e381c003f2b79fb149fc7ea7
SeungWooHan <peter001019@naver.com>
2025-11-02 23:43:03 +0900

    25.11.01

---
+++ 한승우/10월 5주차/택배상자.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> order) {
+    int answer = 0;
+    stack<int> st;
+    
+    for(int i = 1; i <= order.size(); i++){
+        st.push(i);
+        
+        while(!st.empty() && order[answer] == st.top()){
+            st.pop();
+            answer++;
+        }
+    }
+    
+    return answer;
+}
